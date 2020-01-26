431bdc1e79f9cfd3b779f6e44d90738dd1b1892d
cllsabino <carlossabino268@gmail.com>
2020-01-26 20:54:34 -0300

    tela de login e rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private router : Router){
+
+  }
+  navegar(){
+    this.router.navigate(['/Login']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginCadastroComponent } from './login-cadastro/login-cadastro.component';
+
+const appRoutes : Routes = [
+  {path : 'Login', component : LoginCadastroComponent}
+ ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginCadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
