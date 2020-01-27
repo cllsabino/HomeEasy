6fa1ce906b46de7f57c11dbc8713b8f706fa3e84
cllsabino <carlossabino268@gmail.com>
2020-01-27 18:38:52 -0300

    component da tela Home e roteamento ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginCadastroComponent } from './login-cadastro/login-cadastro.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes : Routes = [
-  {path : 'Login', component : LoginCadastroComponent}
+  {path : '', redirectTo: 'home', pathMatch: 'full'},
+  {path : 'login', component : LoginCadastroComponent},
+  {path : 'home', component : HomeComponent}
  ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginCadastroComponent
+    LoginCadastroComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
