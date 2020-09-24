1a30dca36ac5f9c5a7fae999abfd0b0267177065
cllsabino <carlossabino268@gmail.com>
2020-09-24 15:14:01 -0300

    rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
@@ -10,20 +10,23 @@ import { LoginCadastroComponent } from './login-cadastro/login-cadastro.componen
 import { HomeComponent } from './home/home.component';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { DuvidasComponent } from './duvidas/duvidas.component';
-import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 import { ContatoComponent } from './contato/contato.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { AlvaroComponentComponent } from './sobre-nos/alvaro-component/alvaro-component.component';
 import { CarlosComponentComponent } from './sobre-nos/carlos-component/carlos-component.component';
 import { PericlesComponentComponent } from './sobre-nos/pericles-component/pericles-component.component';
-import { AlvaroComponentComponent } from './sobre-nos/alvaro-component/alvaro-component.component';
 
 const appRoutes : Routes = [
   {path : '', redirectTo: 'home', pathMatch: 'full'},
   {path : 'login', component : LoginCadastroComponent},
   {path : 'home', component : HomeComponent},
-  {path : 'sobrenos', component : SobreNosComponent},
   {path : 'duvidas', component : DuvidasComponent},
-  {path : 'contato', component : ContatoComponent}
- ];
+  {path : 'contato', component : ContatoComponent},
+  {path : 'sobrenos', component : SobreNosComponent},
+  {path : 'alvaro', component : AlvaroComponentComponent},
+  {path : 'carlos', component : CarlosComponentComponent},
+  {path : 'pericles', component : PericlesComponentComponent}
+];
 
 @NgModule({
   declarations: [
@@ -31,19 +34,19 @@ const appRoutes : Routes = [
     LoginCadastroComponent,
     HomeComponent,
     DuvidasComponent,
-    SobreNosComponent,
     ContatoComponent,
+    SobreNosComponent,
+    AlvaroComponentComponent,
     CarlosComponentComponent,
-    PericlesComponentComponent,
-    AlvaroComponentComponent
+    PericlesComponentComponent
   ],
   imports: [
-    BrowserModule,
+  BrowserModule,
     FormsModule,
     RouterModule.forRoot(appRoutes), 
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
   ], 
   providers: [],
   bootstrap: [AppComponent],
--- src/app/sobre-nos/sobre-nos.component.ts
+++ src/app/sobre-nos/sobre-nos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-sobre-nos',
   templateUrl: './sobre-nos.component.html',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobreNosComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    
+  ) { }
 
   ngOnInit() {
   }
+
 
 }
