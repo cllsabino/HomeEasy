f55008d0f54534b63e628a1b8b47d49518c07312
cllsabino <carlossabino268@gmail.com>
2020-12-16 10:59:50 -0300

    a ***** do heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCYyutJVjVj9HxXm385h6Jtcc1rkuQ5GYg",
+    authDomain: "homeeasy-52792.firebaseapp.com",
+    databaseURL: "https://homeeasy-52792.firebaseio.com",
+    projectId: "homeeasy-52792",
+    storageBucket: "homeeasy-52792.appspot.com",
+    messagingSenderId: "80905431352",
+    appId: "1:80905431352:web:198adf80e4cb1a681179cd",
+    measurementId: "G-M13ZZ6YYV5"
+  }
 };
