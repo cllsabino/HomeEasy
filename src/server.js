b7aa33c27144eaec798753d4f889dd7887954406
Carlos Sabino <49320775+cllsabino@users.noreply.github.com>
2020-12-16 13:16:52 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/hoomeeasy'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/hoomeeasy/index.html'));
+res.sendFile(’index.html’, {root: 'dist/hoomeeasy/’});
 });
 
 // Inicia a aplicação pela porta configurada
