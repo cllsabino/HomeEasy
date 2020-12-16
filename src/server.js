899ee30be14303b70feab137e01561c1fcd16cdd
Carlos Sabino <49320775+cllsabino@users.noreply.github.com>
2020-12-16 13:18:55 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/hoomeeasy'));
+app.use(express.static('./dist/hoomeeasy'));
 
 app.get('/*', function(req,res) {
     
