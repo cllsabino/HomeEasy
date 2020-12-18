0e2b407e3bfe3c278d0747c6b9e6728a26d4510c
Carlos Sabino <49320775+cllsabino@users.noreply.github.com>
2020-12-17 21:32:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/homeeasy'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname + '.', 'dist', 'index.html'));
+res.sendFile('index.html');
 });
 
 // Inicia a aplicação pela porta configurada
